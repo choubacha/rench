724d5a47f4bd0fa629de4ef27d34b3ea6cfbf543
Kevin Choubacha <chewbacha@gmail.com>
2018-01-30 15:27:44 -0800

    Add thread safe channels for accumulating facts
    Builds a channel for broadcasting every fact to the receiving thread.
    This enables us to allocate and push onto only a single vector without
    any resizing.

--- src/main.rs
+++ src/main.rs
@@ -5,35 +5,38 @@ use clap::{Arg, App};
 use reqwest::{Request, Method, Client};
 use std::time::{Instant, Duration};
 use std::thread;
+use std::sync::mpsc::{channel, Sender};
 
 mod stats;
 use stats::{Fact, Summary};
 
 
-fn make_requests(url: &str, number_of_requests: u32) -> Vec<Fact> {
+fn make_requests(urls: Vec<String>, number_of_requests: usize, sender: Sender<Option<Fact>>) {
     let client = Client::new();
 
-    // Warm up
-    let request = Request::new(Method::Get, url.parse().expect("Invalid url"));
-    let _ = client.execute(request).expect("Failure to warm connection");
+    (0..number_of_requests).for_each(|n| {
+        let url = &urls[n % urls.len()];
 
-    (0..number_of_requests)
-        .map(|_| {
-            let request = Request::new(Method::Get, url.parse().expect("Invalid url"));
-            let (resp, duration) = time_it(|| {
-                let mut resp = client.execute(request).expect(
-                    "Failure to even connect is no good",
-                );
-                let _ = resp.text().expect("Read the body");
-                resp
-            });
-            Fact::record(resp, duration)
-        })
-        .collect()
+        let request = Request::new(Method::Get, url.parse().expect("Invalid url"));
+        let (resp, duration) = time_it(|| {
+            let mut resp = client.execute(request).expect(
+                "Failure to even connect is no good",
+            );
+            let _ = resp.text().expect("Read the body");
+            resp
+        });
+        sender
+            .send(Some(Fact::record(resp, duration)))
+            .expect("to send the fact correctly");
+    });
+    sender
+        .send(None)
+        .expect("to send None correctly");
 }
 
 fn time_it<F, U>(f: F) -> (U, Duration)
-    where F: FnOnce() -> U
+where
+    F: FnOnce() -> U,
 {
     let start = Instant::now();
     (f(), start.elapsed())
@@ -42,43 +45,59 @@ fn time_it<F, U>(f: F) -> (U, Duration)
 fn main() {
     let matches = App::new("Git Release Names")
         .author("Kevin Choubacha <chewbacha@gmail.com>")
-        .arg(Arg::with_name("URL").required(true))
+        .arg(Arg::with_name("URL").required(true).multiple(true))
         .arg(Arg::with_name("concurrency").short("c").takes_value(true))
         .arg(Arg::with_name("requests").short("n").takes_value(true))
         .get_matches();
 
-    let url = matches
-        .value_of("URL")
-        .expect("URL is required")
-        .to_string();
+    let urls: Vec<String> = matches
+        .values_of("URL")
+        .expect("URLs are required")
+        .map(|v| v.to_string())
+        .collect();
 
     let threads = matches
         .value_of("concurrency")
         .unwrap_or("1")
-        .parse::<u32>()
+        .parse::<usize>()
         .expect("Expected valid number for threads");
 
     let requests = matches
         .value_of("requests")
         .unwrap_or("1000")
-        .parse::<u32>()
+        .parse::<usize>()
         .expect("Expected valid number for number of requests");
 
-    let handles: Vec<thread::JoinHandle<Vec<Fact>>> = (0..threads)
+    let (sender, receiver) = channel::<Option<Fact>>();
+    let mut facts: Vec<Fact> = Vec::with_capacity(requests);
+
+    let handles: Vec<thread::JoinHandle<()>> = (0..threads)
         .map(|_| {
-            let param = url.clone();
-            thread::spawn(move || make_requests(&param, requests / threads))
+            let urls: Vec<String> = urls.clone();
+            let tx = sender.clone();
+            thread::spawn(move || make_requests(urls, requests / threads, tx))
         })
         .collect();
-    let (facts, duration): (Vec<Vec<Fact>>, Duration) = time_it(|| {
-        handles.into_iter().map(|h| h.join().unwrap()).collect()
+
+    let rec_handle = thread::spawn(move || {
+        let mut threads_finished = 0;
+        while threads_finished < threads {
+            if let Some(fact) = receiver.recv().expect("To receive correctly") {
+                facts.push(fact);
+            } else {
+                threads_finished += 1;
+            }
+        }
+        facts
     });
+
+    let ((), duration) = time_it(|| {
+        handles.into_iter().for_each(|h| h.join().expect("Sending thread to finish"));
+    });
+    let facts = rec_handle.join().expect("Receiving thread to finish");
+
     let seconds = duration.as_secs() as f64 + (duration.subsec_nanos() as f64 / 1_000_000_000f64);
     println!("Took {} seconds", seconds);
     println!("{} requests / second", requests as f64 / seconds);
-
-    let mut flat_facts: Vec<Fact> = Vec::new();
-    facts.into_iter().for_each(|facts| flat_facts.extend(facts));
-
-    println!("{:?}", Summary::from_facts(&flat_facts));
+    println!("{:?}", Summary::from_facts(&facts));
 }
