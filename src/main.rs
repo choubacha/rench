751f27aac572953bce1183d241d9924097c64dd7
Kevin Choubacha <chewbacha@gmail.com>
2018-02-03 11:32:22 -0800

    Distribute remaining work across all the threads
    When a user selects a number of requests we should execute exactly that
    number. However, the number of requests was subject to rounding errors
    because of issues with the integer division so the remainder was not
    being executed. Now it properly distributes the remainder across all the
    threads.
    closes #8

--- src/main.rs
+++ src/main.rs
@@ -41,6 +41,35 @@ where
     (f(), start.elapsed())
 }
 
+fn distribute_work(threads: usize, requests: usize) -> Vec<usize> {
+    // Every thread should get even work:
+    let base_work = requests / threads;
+    let remaining_work = requests % threads;
+
+    (0..threads)
+        .map(|thread| {
+            // The remainder means that we don't have enough for
+            // every thread to get 1. So we just add one until
+            // we've used up the entire remainder
+            if thread < remaining_work {
+                base_work + 1
+            } else {
+                base_work
+            }
+        })
+        .collect()
+}
+
+#[test]
+fn it_can_distribute_all_work_as_evenly_as_possible() {
+    assert_eq!(distribute_work(3, 1000), vec![334, 333, 333]);
+    assert_eq!(distribute_work(2, 1000), vec![500, 500]);
+    assert_eq!(
+        distribute_work(20, 39),
+        vec![2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1]
+    );
+}
+
 fn main() {
     let matches = App::new("Git Release Names")
         .author("Kevin Choubacha <chewbacha@gmail.com>")
@@ -86,11 +115,12 @@ fn main() {
     });
 
     println!("Beginning requests");
-    let handles: Vec<thread::JoinHandle<()>> = (0..threads)
-        .map(|_| {
+    let handles: Vec<thread::JoinHandle<()>> = distribute_work(threads, requests)
+        .into_iter()
+        .map(|work| {
             let urls: Vec<String> = urls.clone();
             let tx = sender.clone();
-            thread::spawn(move || make_requests(urls, requests / threads, tx))
+            thread::spawn(move || make_requests(urls, work, tx))
         })
         .collect();
 
