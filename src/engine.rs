6527689dd45d27160ab05fbe1fc5cb1b338ac7de
Kevin Choubacha <chewbacha@gmail.com>
2018-02-10 11:13:31 -0800

    Add engine support for hyper
    In order to compare reqwest to hyper I wanted to have both inside the
    same compiled binary. I also wanted to be able to specify which engine
    to use so I can run it twice. To do this I did the following:
    
    1. Changed Facts to no longer be dependent on the reqwest types
    2. Implemented a hyper enginer function
    3. Set up an engine argument for specifying which engine to use.
    
    In the end, this pulls in a few more dependencies but provided a clear
    methodology for running either client.

--- src/engine.rs
+++ src/engine.rs
@@ -1,14 +1,18 @@
 use bench;
 use stats::Fact;
+use content_length::ContentLength;
 
+#[derive(Clone)]
 pub struct Engine {
     urls: Vec<String>,
     requests: usize,
     kind: EngineKind,
 }
 
+#[derive(Clone)]
 enum EngineKind {
     Reqwest,
+    Hyper,
 }
 
 impl Engine {
@@ -20,14 +24,20 @@ impl Engine {
         }
     }
 
+    pub fn with_hyper(mut self) -> Self {
+        self.kind = EngineKind::Hyper;
+        self
+    }
+
     pub fn run<F>(self, f: F) where F: FnMut(Fact) {
         match &self.kind {
             &EngineKind::Reqwest => self.run_reqwest(f),
+            &EngineKind::Hyper => self.run_hyper(f),
         };
     }
 
     fn run_reqwest<F>(&self, mut f: F) where F: FnMut(Fact) {
-        use reqwest::{Client, Method, Request};
+        use reqwest::{header, Client, Method, Request};
         let client = Client::new();
 
         for n in 0..self.requests {
@@ -41,7 +51,44 @@ impl Engine {
                 let _ = resp.text();
                 resp
             });
-            f(Fact::record(resp, duration));
+            let len = resp.headers()
+                .get::<header::ContentLength>()
+                .map(|len| **len)
+                .unwrap_or(0);
+
+            f(Fact::record(ContentLength::new(len), resp.status().as_u16(), duration));
+        }
+    }
+
+    fn run_hyper<F>(&self, mut f: F) where F: FnMut(Fact) {
+        use hyper::{header, Client, Request, Method, Uri};
+        use hyper_tls::HttpsConnector;
+        use tokio_core::reactor::Core;
+        use futures::{Future, Stream};
+
+        let mut core = Core::new().expect("Setting up tokio core failed");
+        let handle = core.handle();
+        let client = Client::configure()
+            .connector(HttpsConnector::new(1, &handle).expect("To set up a http connector"))
+            .build(&handle);
+
+        let urls: Vec<Uri> = self.urls.iter().map(|url| url.parse().unwrap()).collect();
+
+        for n in 0..self.requests {
+            let uri = &urls[n % urls.len()];
+            let request = client.request(Request::new(Method::Get, uri.clone()))
+                .and_then(|response| {
+                    let status = response.status().as_u16();
+                    let content_length = response.headers()
+                        .get::<header::ContentLength>()
+                        .map(|len| len.0)
+                        .unwrap_or(0);
+                    response.body()
+                        .concat2()
+                        .map(move |_| (status, content_length))
+                 });
+            let ((status, content_length), duration) = bench::time_it(|| core.run(request).expect("reactor run"));
+            f(Fact::record(ContentLength::new(content_length), status, duration));
         }
     }
 }
@@ -53,6 +100,14 @@ mod tests {
     #[test]
     fn reqwest_engine_can_collect_facts() {
         let eng = Engine::new(vec!["https://www.google.com".to_string()], 1);
+        let mut fact: Option<Fact> = None;
+        eng.run(|f| fact = Some(f));
+        assert!(fact.is_some());
+    }
+
+    #[test]
+    fn hyper_engine_can_collect_facts() {
+        let eng = Engine::new(vec!["https://www.google.com".to_string()], 1).with_hyper();
         let mut fact: Option<Fact> = None;
         eng.run(|f| fact = Some(f));
         assert!(fact.is_some());
